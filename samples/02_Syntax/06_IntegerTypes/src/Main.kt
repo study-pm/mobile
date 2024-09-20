ddbcf8e13331ca1d5b8609907e9274265a81068e
viktor <sterilework@gmail.com>
2024-09-20 23:42:15 +0300

    docs(samples/02): add sample 6

---
+++ samples/02_Syntax/06_IntegerTypes/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var testByte: Byte = 120
+    var testShort: Short = 32000
+    var testInt: Int = 957_235_455
+    var testLong: Long = 873_554_554_545_525
+    var testLongTwo: Long = 934_347_523_555L
+}
