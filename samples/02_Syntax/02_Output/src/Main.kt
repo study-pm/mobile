731b625d13ea720c6c8e1f7e2edebb50befacd48
viktor <sterilework@gmail.com>
2024-09-20 20:24:29 +0300

    docs(samples/02): add sample for 02

---
+++ samples/02_Syntax/02_Output/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val testF = "1234567"
+    for (i in 0..testF.length - 1) {
+        println(testF[i])
+    }
+    println("====================")
+    for (i in 0..testF.length - 1) {
+        print(testF[i])
+    }
+}
