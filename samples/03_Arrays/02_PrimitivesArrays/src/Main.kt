4a213f84fdb5ecf567a5332aee686948e3270b1e
viktor <sterilework@gmail.com>
2024-09-25 18:48:31 +0300

    docs(samples/03): add sample 2

---
+++ samples/03_Arrays/02_PrimitivesArrays/src/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val arrInt = intArrayOf(9, 6, 3, 6, 1)
+    val arrDouble = doubleArrayOf(3.7, 3.4, 9.1)
+    val arrLogic = booleanArrayOf(true, true, false)
+    val arrFloatOne = floatArrayOf(2F, 5.78F, 78.91F, 105F)
+    // val arrFloatTwo = floatArrayOf(2, 5.78, 78.91, 105) // => Kotlin: Argument type mismatch
+    val arrChar = charArrayOf('E', 'G', 'k', 'p', 'Z')
+    val arrLongOne = longArrayOf(78L, 62L, 100_456_891L)
+    val arrLongTwo = longArrayOf(78, 62, 100_456_891)
+    val arrByte = byteArrayOf(-23, 127, 17, -98)
+    val arrShort = shortArrayOf(-32000, 4500, 491, -9837)
+}
