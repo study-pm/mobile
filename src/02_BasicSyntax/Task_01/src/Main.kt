4e428426bc1d1d0dfc27c1e4f5813a34515c31f6
viktor <sterilework@gmail.com>
2024-10-05 18:11:19 +0300

    feat(src/02): add problem 01 solution

---
+++ src/02_BasicSyntax/Task_01/src/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+    print("Enter character sequence: ")
+    val input = readln()
+
+    if (input.isEmpty()) {
+        println("The input is empty")
+        return
+    }
+
+    var res = ""
+
+    var count = 1
+    var lastChar: Char? = null
+
+    for (c in input) {
+        if (c == lastChar) count++
+        else {
+            res += if (count != 1) count else ""
+            res += c
+            lastChar = c
+            count = 1
+        }
+    }
+    if (count > 1) res += count
+
+    println(res)
+}
