821b58c97215ff9a7268971da2a3825f67267a73
viktor <sterilework@gmail.com>
2024-10-05 19:39:52 +0300

    feat(src/02): add problem 2 solution

---
+++ src/02_BasicSyntax/Task_02/src/Main.kt
@@ -0,0 +1,18 @@
+/**
+ * Returns the symbol occurrences count map in the order of their appearance in the original string
+ * @param s the input string
+ * @return unsorted map of symbols count
+ */
+fun getSymbolCount(s: String): MutableMap<Char, Int> {
+    val res = mutableMapOf<Char, Int>()
+    for (c in s) res[c] = res.getOrElse(c) { 0 } + 1
+    return res
+}
+
+fun main() {
+    print("Enter character sequence: ")
+    val input = readln()
+
+    val resMap = getSymbolCount(input).toSortedMap()
+    resMap.forEach { println("${it.key} - ${it.value}") }
+}
