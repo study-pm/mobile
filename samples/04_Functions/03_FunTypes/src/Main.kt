8d6000c5fd370cd37f2794fbccb719607a317d7b
viktor <sterilework@gmail.com>
2024-11-28 15:25:00 +0300

    docs: add sample 03

---
+++ samples/04_Functions/03_FunTypes/src/Main.kt
@@ -0,0 +1,10 @@
+import kotlin.math.log
+import kotlin.math.sqrt
+
+fun main() {
+    val number = 81
+    println(sqrt(number.toFloat()))
+
+    val base = 10
+    println(log(1000F, base.toFloat()))
+}
